c227a79ae03e9daeab90abf92b95812016303713
rpmiller18 <rpmiller@udel.edu>
2021-10-12 21:19:11 -0400

    branch commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 UD CIS Scheduler Updated
-                Ryan Miller Connor Creavin and Usama
+                Ryan Miller Connor Creavin and Usama Mahmood
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
