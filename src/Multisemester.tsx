7a5fb21a8f8745a58ce544781b47557b997e0185
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-10-19 12:43:23 -0400

    Once again, recoded to account for scalability

--- src/Multisemester.tsx
+++ src/Multisemester.tsx
@@ -1,80 +1,13 @@
 import React from "react";
-//import logo from "./logo.svg";
 import "./App.css";
-//import "bootstrap/dist/css/bootstrap.min.css";
+import OneSemester from "./OneSemester";
 
 function MultiSemester(): JSX.Element {
     return (
         <div className="container-fluid padding">
             <div className="row padding">
 
-                <div className="col-md-12 col-lg-6">
-                    <h2>Fall Semester</h2>
-
-                    <div className="row padding">
-                        <div className="col-md-8">
-                            <h3>Course</h3>
-                        </div>
-                        <div className="col-md-4">
-                            <h3>Credits</h3>
-                        </div>
-                    </div>
-
-                    <div className="row padding">
-                        <div className="col-md-8">
-                            <p>
-                                <b>EGGG 101</b> Introduction to Engineering
-                            </p>
-                        </div>
-                        <div className="col-md-4">
-                            <p>2</p>
-                        </div>
-                    </div>
-
-                    <div className="row padding">
-                        <div className="col-md-8">
-                            <p>
-                                <b>CISC 108</b> Introduction to Computer Science I
-                            </p>
-                        </div>
-                        <div className="col-md-4">
-                            <p>3</p>
-                        </div>
-                    </div>
-
-                    <div className="row padding">
-                        <div className="col-md-8">
-                            <p>
-                                <b>MATH 241</b> Analytic Geometry and Calculus A
-                            </p>
-                        </div>
-                        <div className="col-md-4">
-                            <p>4</p>
-                        </div>
-                    </div>
-
-                    <div className="row padding">
-                        <div className="col-md-8">
-                            <p>
-                                <b>ENGL 110</b> Seminar in Composition
-                            </p>
-                        </div>
-                        <div className="col-md-4">
-                            <p>3</p>
-                        </div>
-                    </div>
-
-                    <div className="row padding">
-                        <div className="col-md-8">
-                            <h5>Total Credits</h5>
-                        </div>
-                        <div className="col-md-4">
-                            <h5>12</h5>
-                        </div>
-                    </div>
-
-                </div>
-
+                <OneSemester />
 
                 <div className="col-md-12 col-lg-6">
                     <h2>Spring Semester</h2>
@@ -111,6 +44,11 @@ function MultiSemester(): JSX.Element {
                 </div>
 
 
+            </div>
+
+            <div className="row padding">
+                <OneSemester />
+                <OneSemester />
             </div>
 
         </div>
---
+++ src/OneSemester.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./App.css";
+
+function OneSemester(): JSX.Element {
+    return (
+        <div className="col-md-12 col-lg-6">
+            <h2>Semester</h2>
+
+            <div className="row padding">
+                <div className="col-md-8">
+                    <h3>Course</h3>
+                </div>
+                <div className="col-md-4">
+                    <h3>Credits</h3>
+                </div>
+            </div>
+
+            <div className="row padding">
+                <div className="col-md-8">
+                    <p>
+                        <b>EGGG 101</b> Introduction to Engineering
+                    </p>
+                </div>
+                <div className="col-md-4">
+                    <p>2</p>
+                </div>
+            </div>
+
+            <div className="row padding">
+                <div className="col-md-8">
+                    <p>
+                        <b>CISC 108</b> Introduction to Computer Science I
+                    </p>
+                </div>
+                <div className="col-md-4">
+                    <p>3</p>
+                </div>
+            </div>
+
+            <div className="row padding">
+                <div className="col-md-8">
+                    <p>
+                        <b>MATH 241</b> Analytic Geometry and Calculus A
+                    </p>
+                </div>
+                <div className="col-md-4">
+                    <p>4</p>
+                </div>
+            </div>
+
+            <div className="row padding">
+                <div className="col-md-8">
+                    <p>
+                        <b>ENGL 110</b> Seminar in Composition
+                    </p>
+                </div>
+                <div className="col-md-4">
+                    <p>3</p>
+                </div>
+            </div>
+
+            <div className="row padding">
+                <div className="col-md-8">
+                    <h5>Total Credits</h5>
+                </div>
+                <div className="col-md-4">
+                    <h5>12</h5>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default OneSemester;
