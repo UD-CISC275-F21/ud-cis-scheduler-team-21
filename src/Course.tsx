695f79019814545d4edacb53b696ea6bff59f716
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-10-20 20:30:15 -0400

    reformatting and adding a semicolon

--- src/Course.tsx
+++ src/Course.tsx
@@ -7,18 +7,18 @@ type CourseIntf = {
     crsDescription: string,
     crsCredits: string
 }
-export const Course = ({crsName, crsDescription, crsCredits}: CourseIntf) => 
+export const Course = ({ crsName, crsDescription, crsCredits }: CourseIntf) =>
     //return (
-        <div className="row padding">
-            <div className="col-md-8">
-                <p>
-                    <b>{crsName}</b> {crsDescription}
-                </p>
-            </div>
-            <div className="col-md-4">
-                <p>{crsCredits}</p>
-            </div>
+    <div className="row padding">
+        <div className="col-md-8">
+            <p>
+                <b>{crsName}</b> {crsDescription}
+            </p>
         </div>
+        <div className="col-md-4">
+            <p>{crsCredits}</p>
+        </div>
+    </div>;
     //);//as JSX.Element;
 //}
 
