0d04528c19055114f360babd4e24486f421dde70
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-10-20 22:35:59 -0400

    Changed Way Courses Were Impimented in OneSemester Display to better represent how we will do it with variables

--- src/Course.tsx
+++ src/Course.tsx
@@ -1,25 +1,22 @@
 import React, { FunctionComponent } from "react";
 import "./App.css";
-//import PropTypes from "prop-types";
 
 type CourseIntf = {
     crsName: string,
     crsDescription: string,
     crsCredits: string
 }
+
+
 export const Course: FunctionComponent<CourseIntf> = ({ crsName, crsDescription, crsCredits }) =>
-    //return (
+
     <div className="row padding">
         <div className="col-md-8">
             <p>
-                <b>{crsName}</b> {crsDescription}
+                <b>{crsName}:</b> {crsDescription}
             </p>
         </div>
         <div className="col-md-4">
             <p>{crsCredits}</p>
         </div>
     </div>;
-    //);//as JSX.Element;
-//}
-
-//export default Course;
--- src/OneSemester.tsx
+++ src/OneSemester.tsx
@@ -3,54 +3,30 @@ import "./App.css";
 import { Course } from "./Course";
 
 function OneSemester(): JSX.Element {
-    //const sem1 = <Course crsName="EGG101" crsDescription="Introduction to Engineering" crsCredits="2"/>;
     return (
         <div className="col-md-6">
             <h2>Semester</h2>
 
             <div className="row padding">
-                <div className="col-md-4">
+                <div className="col-md-8">
                     <h3>Course</h3>
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-4">
                     <h3>Credits</h3>
                 </div>
             </div>
 
-            <Course crsName="EGG101" crsDescription="Introduction to Engineering" crsCredits="2"/>
 
-            <div className="row padding">
-                <div className="col-md-8">
-                    <p>
-                        <b>CISC 108</b> Introduction to Computer Science I
-                    </p>
-                </div>
-                <div className="col-md-4">
-                    <p>3</p>
-                </div>
-            </div>
+            <Course crsName="EGGG 101" crsDescription="Introduction to Engineering" crsCredits="2"/>
 
-            <div className="row padding">
-                <div className="col-md-8">
-                    <p>
-                        <b>MATH 241</b> Analytic Geometry and Calculus A
-                    </p>
-                </div>
-                <div className="col-md-4">
-                    <p>4</p>
-                </div>
-            </div>
+            <Course crsName="CISC 108" crsDescription="Introduction to Computer Science I" crsCredits="3"/>
 
-            <div className="row padding">
-                <div className="col-md-8">
-                    <p>
-                        <b>ENGL 110</b> Seminar in Composition
-                    </p>
-                </div>
-                <div className="col-md-4">
-                    <p>3</p>
-                </div>
-            </div>
+            <Course crsName="MATH 241" crsDescription="Analytic Geometry and Calculus A" crsCredits="4"/>
+
+            <Course crsName="ENGL 110" crsDescription="Seminar in Composition" crsCredits="3"/>
+
+            <Course crsName="Breadth Requirement I" crsDescription="Breadth Elective of Required Category" crsCredits="3"/>
+
 
             <div className="row padding">
                 <div className="col-md-8">
