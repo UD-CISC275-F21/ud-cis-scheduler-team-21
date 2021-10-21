c813af574dfcf51c7c5df2ff32234cb6a96933b0
usamachaudhary233@gmail.com <usamachaudhary233@gmail.com>
2021-10-20 22:19:59 -0400

    check

--- src/Multisemester.tsx
+++ src/Multisemester.tsx
@@ -1,44 +1,53 @@
 import React from "react";
-//import logo from "./logo.svg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import OneSemester from "./OneSemester";
 
 function MultiSemester(): JSX.Element {
     return (
         <div className="container-fluid padding">
             <div className="row padding">
+
+
                 <div className="col-md-12 col-lg-6">
-                    <h2>Fall Semester</h2>
+                    <h2>Spring Semester</h2>
+
                     <div className="row padding">
-                        <div className="col-md-8 col-lg-4">
+                        <div className="col-md-8 col-lg-8">
                             <h3>Course</h3>
                             <p>
-                                <h2>EGGG 101</h2> Introduction to Engineering
+                                <b>CISC 181</b> Introduction to Computer Science II
                             </p>
                             <p>
-                                <h2>CISC 108</h2> Introduction to Computer Science I
+                                <b>CISC 210</b> Introduction to Systems Programming
                             </p>
                             <p>
-                                <h2>MATH 241</h2> Analytic Geometry and Calculus A
+                                <b>MATH 242</b> Calculus B and Calculus C
                             </p>
                             <p>
-                                <h2>ENGL 110</h2> Seminar in Composition
+                                <b>Breadth</b> Breadth Requirement (1/5)
                             </p>
                             <p>
-                                <h2>Total Credits:</h2>
+                                <b>Total Credits:</b>
                             </p>
                         </div>
-                        <div className="col-md-4 col-lg-2">
+
+                        <div className="col-md-4 col-lg-4">
                             <h3>Credits</h3>
-                            <p>2</p>
+                            <p>3</p>
                             <p>3</p>
                             <p>4</p>
-                            <p>3</p>
-                            <p>12</p>
+                            <p>?</p>
+                            <p>10+?</p>
                         </div>
                     </div>
                 </div>
+
+
             </div>
+
+            <div className="row padding">
+            </div>
+
         </div>
 
     );
