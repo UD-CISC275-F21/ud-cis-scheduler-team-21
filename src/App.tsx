1b73d963fa6740b6163547812161204abe1395e8
usamachaudhary233@gmail.com <usamachaudhary233@gmail.com>
2021-10-22 13:05:06 -0400

    added custom button for editing single semester view

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,26 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 <h1 >UD CIS Scheduler</h1>
-                
             </header>
+
             <div className="text-left"><img className="logo" src={logo} alt="Logo"/></div>
+
             <div className="text-right">
                 <button type="button" className="btn btn-primary m-3 ">Export PDF</button>
             </div>
+
+            <div className="custom">
+                <button type="button" className="btn btn-primary m-3 ">Custom</button>
+            </div>
+
             <MultiSemester />
+            
             <div className="col text-center">
                 <button type="button" className="btn btn-primary btn-lg m-3">Add Semester</button>
                 <button type="button" className="btn btn-danger btn-lg m-3">Remove All</button>
                 <button type="button" className="btn btn-secondary btn-lg m-3">Reset</button>
             </div>
+            
         </div>
         
     );
