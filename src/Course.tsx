c989e4b430d804b3d1fbd8d1b018799317e6e370
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-10-25 06:54:24 -0400

    Changed the format of semesters and courses to make it a table instead of divs

--- src/Course.tsx
+++ src/Course.tsx
@@ -10,13 +10,7 @@ export type CourseIntf = {
 
 export const Course: FunctionComponent<CourseIntf> = ({ crsName, crsDescription, crsCredits }) =>
 
-    <div className="row padding">
-        <div className="col-md-8">
-            <p>
-                <b>{crsName}</b> {crsDescription}
-            </p>
-        </div>
-        <div className="col-md-4">
-            <p>{crsCredits}</p>
-        </div>
-    </div>;
+    <tr>
+        <td>{crsName} {crsDescription}</td>
+        <td>{crsCredits}</td>
+    </tr>;
