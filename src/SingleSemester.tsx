976859a3df9af8fb6c9410a920a2cfef18709abc
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-10-25 17:44:44 -0400

    made single semester view scrollable through all available semesters

--- src/SingleSemester.tsx
+++ src/SingleSemester.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Semester_SS } from "./OneSemester";
 import { semester_list } from "./Globals";
 
 function SingleSemester(): JSX.Element {
 
-    const current_semester_num = 1;
+    const[focused_semester, updateFocus] = useState(<Semester_SS course_1={semester_list[0].course_1}
+        course_2={semester_list[0].course_2}
+        course_3={semester_list[0].course_3}
+        course_4={semester_list[0].course_4}
+        course_5={semester_list[0].course_5}
+        semester_number={semester_list[0].semester_number}
+    />);
+
+    let current_semester_num = 1;
+
+    const next_click=()=>{
+        if(current_semester_num<8){
+            current_semester_num = current_semester_num + 1;
+            updateFocus(<Semester_SS course_1={semester_list[(current_semester_num-1)].course_1}
+                course_2={semester_list[(current_semester_num-1)].course_2}
+                course_3={semester_list[(current_semester_num-1)].course_3}
+                course_4={semester_list[(current_semester_num-1)].course_4}
+                course_5={semester_list[(current_semester_num-1)].course_5}
+                semester_number={semester_list[(current_semester_num-1)].semester_number}
+            />);
+        }
+    }
+    const prev_click=()=>{
+        if(current_semester_num>1){
+            current_semester_num = current_semester_num - 1;
+            updateFocus(<Semester_SS course_1={semester_list[(current_semester_num-1)].course_1}
+                course_2={semester_list[(current_semester_num-1)].course_2}
+                course_3={semester_list[(current_semester_num-1)].course_3}
+                course_4={semester_list[(current_semester_num-1)].course_4}
+                course_5={semester_list[(current_semester_num-1)].course_5}
+                semester_number={semester_list[(current_semester_num-1)].semester_number}
+            />);
+        }
+    }
 
     return (
 
@@ -13,24 +46,18 @@ function SingleSemester(): JSX.Element {
 
             <div className="row padding">
                 <div className="col-sm-2 col-md-3 col-lg-4 text-center">
-                    <button type="button" className="btn btn-primary m-3 ">Previous Semester</button>
+                    <button type="button" className="btn btn-primary m-3" onClick={() => prev_click()}>Previous Semester</button>
                 </div>
                 <div className="col-sm-2 col-md-3 col-lg-4 text-center">
                     <h3>Semester #{current_semester_num}</h3>
                 </div>
                 <div className="col-sm-2 col-md-3 col-lg-4 text-center">
-                    <button type="button" className="btn btn-primary m-3 ">Next Semester</button>
+                    <button type="button" className="btn btn-primary m-3" onClick={() => next_click()}>Next Semester</button>
                 </div>
             </div>
 
 
-            <Semester_SS course_1={semester_list[0].course_1}
-                course_2={semester_list[0].course_2}
-                course_3={semester_list[0].course_3}
-                course_4={semester_list[0].course_4}
-                course_5={semester_list[0].course_5}
-                semester_number={semester_list[0].semester_number}
-            />
+            {focused_semester}
 
         </div>
     );
