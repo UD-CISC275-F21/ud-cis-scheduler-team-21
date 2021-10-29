0262c5d62fac7100f3c8c459579130e4d0a32ea2
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-10-29 04:46:49 -0400

    changed the semester type to include an array of classes instead of individual classes

--- src/Multisemester.tsx
+++ src/Multisemester.tsx
@@ -13,19 +13,11 @@ function MultiSemester(): JSX.Element {
                 <div className="Year">
                     <h4>First Year</h4>
                 </div>
-                <Semester_MS course_1={semester_list[0].course_1}
-                    course_2={semester_list[0].course_2}
-                    course_3={semester_list[0].course_3}
-                    course_4={semester_list[0].course_4}
-                    course_5={semester_list[0].course_5}
+                <Semester_MS course_set={semester_list[0].course_set}
                     semester_number={semester_list[0].semester_number}
                 />
 
-                <Semester_MS course_1={semester_list[1].course_1}
-                    course_2={semester_list[1].course_2}
-                    course_3={semester_list[1].course_3}
-                    course_4={semester_list[1].course_4}
-                    course_5={semester_list[1].course_5}
+                <Semester_MS course_set={semester_list[1].course_set}
                     semester_number={semester_list[1].semester_number}
                 />
             </div>
@@ -35,19 +27,11 @@ function MultiSemester(): JSX.Element {
                     <h4>Second Year</h4>
                 </div>
 
-                <Semester_MS course_1={semester_list[2].course_1}
-                    course_2={semester_list[2].course_2}
-                    course_3={semester_list[2].course_3}
-                    course_4={semester_list[2].course_4}
-                    course_5={semester_list[2].course_5}
+                <Semester_MS course_set={semester_list[2].course_set}
                     semester_number={semester_list[2].semester_number}
                 />
 
-                <Semester_MS course_1={semester_list[3].course_1}
-                    course_2={semester_list[3].course_2}
-                    course_3={semester_list[3].course_3}
-                    course_4={semester_list[3].course_4}
-                    course_5={semester_list[3].course_5}
+                <Semester_MS course_set={semester_list[3].course_set}
                     semester_number={semester_list[3].semester_number}
                 />
             </div>
@@ -57,19 +41,11 @@ function MultiSemester(): JSX.Element {
                     <h4>Third Year</h4>
                 </div>
 
-                <Semester_MS course_1={semester_list[4].course_1}
-                    course_2={semester_list[4].course_2}
-                    course_3={semester_list[4].course_3}
-                    course_4={semester_list[4].course_4}
-                    course_5={semester_list[4].course_5}
+                <Semester_MS course_set={semester_list[4].course_set}
                     semester_number={semester_list[4].semester_number}
                 />
 
-                <Semester_MS course_1={semester_list[5].course_1}
-                    course_2={semester_list[5].course_2}
-                    course_3={semester_list[5].course_3}
-                    course_4={semester_list[5].course_4}
-                    course_5={semester_list[5].course_5}
+                <Semester_MS course_set={semester_list[5].course_set}
                     semester_number={semester_list[5].semester_number}
                 />
             </div>
@@ -80,19 +56,11 @@ function MultiSemester(): JSX.Element {
                     <h4>Fourth Year</h4>
                 </div>
 
-                <Semester_MS course_1={semester_list[6].course_1}
-                    course_2={semester_list[6].course_2}
-                    course_3={semester_list[6].course_3}
-                    course_4={semester_list[6].course_4}
-                    course_5={semester_list[6].course_5}
+                <Semester_MS course_set={semester_list[6].course_set}
                     semester_number={semester_list[6].semester_number}
                 />
 
-                <Semester_MS course_1={semester_list[7].course_1}
-                    course_2={semester_list[7].course_2}
-                    course_3={semester_list[7].course_3}
-                    course_4={semester_list[7].course_4}
-                    course_5={semester_list[7].course_5}
+                <Semester_MS course_set={semester_list[7].course_set}
                     semester_number={semester_list[7].semester_number}
                 />
             </div>
