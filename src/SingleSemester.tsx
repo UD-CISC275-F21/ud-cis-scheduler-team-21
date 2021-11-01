a4a547acfe12e1c6ee70c9d067ed76f6fc96e3ba
usamachaudhary233@gmail.com <usamachaudhary233@gmail.com>
2021-11-01 18:34:32 -0400

    check

--- src/SingleSemester.tsx
+++ src/SingleSemester.tsx
@@ -5,42 +5,38 @@ import { semester_list } from "./Globals";
 
 function SingleSemester(): JSX.Element {
 
-    const[focused_semester, updateFocus] = useState(<Semester_SS course_1={semester_list[0].course_1}
-        course_2={semester_list[0].course_2}
-        course_3={semester_list[0].course_3}
-        course_4={semester_list[0].course_4}
-        course_5={semester_list[0].course_5}
+    const [focused_semester, updateFocus] = useState(<Semester_SS course_set={semester_list[0].course_set}
         semester_number={semester_list[0].semester_number}
     />);
 
     const [current_semester_num, changeSemNum] = useState(0);
 
-    const next_click=()=>{
-        if(current_semester_num<7){
-            changeSemNum(v=>v+1);
+    const next_click = () => {
+        if (current_semester_num < 7) {
+            changeSemNum(v => v + 1);
             console.log(current_semester_num);
-            updateFocus(<Semester_SS course_1={semester_list[current_semester_num+1].course_1}
-                course_2={semester_list[current_semester_num+1].course_2}
-                course_3={semester_list[current_semester_num+1].course_3}
-                course_4={semester_list[current_semester_num+1].course_4}
-                course_5={semester_list[current_semester_num+1].course_5}
-                semester_number={semester_list[current_semester_num+1].semester_number}
+            updateFocus(<Semester_SS course_set={semester_list[current_semester_num + 1].course_set}
+                semester_number={semester_list[current_semester_num + 1].semester_number}
             />);
         }
     };
-    const prev_click=()=>{
-        if(current_semester_num>0){
-            changeSemNum(v=>v-1);
+    const prev_click = () => {
+        
+        if (current_semester_num > 0) {
+            changeSemNum(v => v - 1);
             console.log(current_semester_num);
-            updateFocus(<Semester_SS course_1={semester_list[current_semester_num-1].course_1}
-                course_2={semester_list[current_semester_num-1].course_2}
-                course_3={semester_list[current_semester_num-1].course_3}
-                course_4={semester_list[current_semester_num-1].course_4}
-                course_5={semester_list[current_semester_num-1].course_5}
-                semester_number={semester_list[current_semester_num-1].semester_number}
+            updateFocus(<Semester_SS course_set={semester_list[current_semester_num - 1].course_set}
+                semester_number={semester_list[current_semester_num - 1].semester_number}
             />);
         }
+        
     };
+
+    /*useEffect(() => {
+        updateFocus(<Semester_SS course_set={semester_list[current_semester_num].course_set}
+            semester_number={semester_list[current_semester_num].semester_number}
+        />);
+    }, [focused_semester]);*/
 
     return (
 
@@ -51,18 +47,18 @@ function SingleSemester(): JSX.Element {
                     <button type="button" className="btn btn-primary m-3" onClick={() => prev_click()}>Previous Semester</button>
                 </div>
                 <div className="col-sm-2 col-md-3 col-lg-4 text-center">
-                    <h3>Semester #{current_semester_num+1}</h3>
+                    <h3>Semester #{current_semester_num + 1}</h3>
                 </div>
                 <div className="col-sm-2 col-md-3 col-lg-4 text-center">
                     <button type="button" className="btn btn-primary m-3" onClick={() => next_click()}>Next Semester</button>
                 </div>
             </div>
 
-            {focused_semester}            
+            {focused_semester}
 
         </div>
 
-        
+
     );
 }
 
