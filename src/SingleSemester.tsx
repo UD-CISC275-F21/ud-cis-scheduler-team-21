fc5165e50c152998d38a45b0c828503401d9f385
usamachaudhary233@gmail.com <usamachaudhary233@gmail.com>
2021-11-10 20:17:17 -0500

    errorrrrrr

--- src/SingleSemester.tsx
+++ src/SingleSemester.tsx
@@ -39,105 +39,102 @@ function SingleSemester(): JSX.Element {
     const remove_semester = () => {
         semester_list.splice(current_semester_num,1);
         if (current_semester_num < semester_list.length) {
-            semester_list.forEach((semester: SemesterIntf, index: number)=>{
-                semester.semester_number = index+1;
-            });
-            
-            if (current_semester_num < semester_list.length-1) {
-                console.log(current_semester_num);
-                updateFocus(<Semester_SS course_set={semester_list[current_semester_num].course_set}
-                    semester_number={semester_list[current_semester_num].semester_number}
-                />);
-            }
+        semester_list.forEach((semester: SemesterIntf, index: number)=>{
+            semester.semester_number = index+1;
+        });
+        
+        if (current_semester_num < semester_list.length-1) {
+            console.log(current_semester_num);
+            updateFocus(<Semester_SS course_set={semester_list[current_semester_num].course_set}
+                semester_number={semester_list[current_semester_num].semester_number}
+            />);
+        }
 
-        };
+    };
 
-        //Removes all the semester
-        const remove_Allsemester = () => {
-            semester_list.splice(0,semester_list.length);
-            if (current_semester_num < semester_list.length-1) {
-                console.log(current_semester_num);
-                updateFocus(<Semester_SS course_set={semester_list[current_semester_num].course_set}
-                    semester_number={semester_list[current_semester_num].semester_number}
-                />);
-            }
-        };
+    //Removes all the semester
+    const remove_Allsemester = () => {
+        semester_list.splice(0,semester_list.length);
+        if (current_semester_num < semester_list.length-1) {
+            console.log(current_semester_num);
+            updateFocus(<Semester_SS course_set={semester_list[current_semester_num].course_set}
+                semester_number={semester_list[current_semester_num].semester_number}
+            />);
+        }
+    };
 
-        const[searchTerm, setSearchTerm] = useState<string>("");
+    const[searchTerm, setSearchTerm] = useState<string>("");
 
+    return (
 
+        <div className="container-fluid padding text-left">
+            <div className="row padding">
+                <div className="col-sm-2 col-md-3 col-lg-4 text-center">
+                    <button type="button" className="btn btn-primary m-3" onClick={() => prev_click()}>Previous Semester</button>
+                    <button type="button" className="btn btn-primary m-3" onClick={() => next_click()}>Next Semester</button>
+                    <button type="button" className="btn btn-danger m-3" onClick={() => remove_semester()}>Remove Semester</button>
+                    <button type="button" className="btn btn-danger m-3" onClick={() => remove_Allsemester()}>Remove All Semesters</button>
 
-
-        return (
-
-            <div className="container-fluid padding text-left">
-                <div className="row padding">
-                    <div className="col-sm-2 col-md-3 col-lg-4 text-center">
-                        <button type="button" className="btn btn-primary m-3" onClick={() => prev_click()}>Previous Semester</button>
-                        <button type="button" className="btn btn-primary m-3" onClick={() => next_click()}>Next Semester</button>
-                        <button type="button" className="btn btn-danger m-3" onClick={() => remove_semester()}>Remove Semester</button>
-                        <button type="button" className="btn btn-danger m-3" onClick={() => remove_Allsemester()}>Remove All Semesters</button>
-
-                    </div>
                 </div>
-                <div className="row">
-                    <div className="col-6">
-                        {focused_semester}
-                    </div>
-
-                    <div className="col-3">
-                        <h2 className="text-center"><b>Add course</b></h2> 
-                        <h5 className="text-center">Add course to the Semester {current_semester_num + 1}</h5>
-                        <div className="text-center">
-                            <input className="text-center" placeholder="Enter Course Code"
-                                onChange={(event)=>{
-                                    setSearchTerm(typeof event.target.value==="string"
-                                        ? event.target.value: "");
-                                }}>
-
-                            </input>
-
-                        </div>
-                        <div className="text-center">
-                            <button type="button" className="btn btn-secondary m-3">Hold</button>
-                            <button type="button" className="btn btn-success m-3">Add</button>
-
-                        </div>
-                        {
-                            Data.filter((post)=>{
-                                if(searchTerm==""){
-                                    return post;
-                                }else if (post.id.toLowerCase().includes(searchTerm.toLowerCase()))
-                                    return post;
-
-                            }).map(post =>{
-                                return(
-                                    <div key={post.id}>
-                                        <h4 className="text-center">{post.id}</h4>
-                                        <p className="text-center">{post.name}</p>
-                                        <p className="text-center">{post.credits}</p>
-                                        <p className="text-left">{post.description}</p>
-                                        
-                                    </div>
-                                );
-                            })
-                        }
-
-                    </div>
-
-                    <div className="col-3">
-                        <h2 className="text-center"><b>Requirements</b></h2>
-                    </div>
-                </div>
-                
-            
-
-
-
             </div>
+            <div className="row">
+                <div className="col-6">
+                    {focused_semester}
+                </div>
+
+                <div className="col-3">
+                    <h2 className="text-center"><b>Add course</b></h2> 
+                    <h5 className="text-center">Add course to the Semester {current_semester_num + 1}</h5>
+                    <div className="text-center">
+                        <input className="text-center" placeholder="Enter Course Code"
+                            onChange={(event)=>{
+                                setSearchTerm(typeof event.target.value==="string"
+                                    ? event.target.value: "");
+                            }}>
+
+                        </input>
+
+                    </div>
+                    <div className="text-center">
+                        <button type="button" className="btn btn-secondary m-3">Hold</button>
+                        <button type="button" className="btn btn-success m-3">Add</button>
+
+                    </div>
+                    {
+                        Data.filter((post)=>{
+                            if(searchTerm==""){
+                                return post;
+                            }else if (post.id.toLowerCase().includes(searchTerm.toLowerCase()))
+                                return post;
+
+                        }).map(post =>{
+                            return(
+                                <div key={post.id}>
+                                    <h4 className="text-center">{post.id}</h4>
+                                    <p className="text-center">{post.name}</p>
+                                    <p className="text-center">{post.credits}</p>
+                                    <p className="text-left">{post.description}</p>
+                                    
+                                </div>
+                            );
+                        })
+                    }
+
+                </div>
+
+                <div className="col-3">
+                    <h2 className="text-center"><b>Requirements</b></h2>
+                </div>
+            </div>
+            
+          
 
 
-        );
+
+        </div>
+
+
+    );
 }
 
 export default SingleSemester;
