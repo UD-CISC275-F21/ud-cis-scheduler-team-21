047ab53368f683b44c5679992972507cb95d8e42
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-11-15 20:18:31 -0500

    fixing linter errors hopefully

--- src/interfaces/Semester.tsx
+++ src/interfaces/Semester.tsx
@@ -1,8 +1,8 @@
 import { Course } from "./Course"
 
 //Created a type to define the traits of a Semester
-export type Semester = {
-    course_set: Course[],
-    semester_number: number,
-    credits?: number
+export interface Semester{
+    course_set: Course[];
+    semester_number: number;
+    credits?: number;
 }
