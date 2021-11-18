e2a827abd183761ae252094070147109369c5939
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-11-18 08:23:58 -0500

    fixing tester errors

--- src/components/WelcomeAndHelp_Pane.tsx
+++ src/components/WelcomeAndHelp_Pane.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function WelcomeAndHelp_Pane() :JSX.Element {
     return (
         <div>
-            <h2>Welsome to Team 21`s UD CIS Scheduler</h2>
+            <h2>Welsome to Team 21`s UD CISC Scheduler</h2>
             <p>Every student at the University of Delaware is assigned an Academic Advisor who is meant to help guide the student to graduation.</p>
             <p>The advisor`s role is to help students become self-sufficient, and they aren`t intended to make decisions on behalf students. Ideally, by the end of their degree, students have chosen their own path to graduation, with minimal input from their adviser. To facilitate their choices, students are often strongly recommended to construct a Degree Plan.</p>
             <p>However, an open-ended field like Computer Science poses a unique challenge, since there are many possible paths to graduation with different tradeoffs and opportunities. Indeed, many students come in with almost no knowledge of the possible career directions and what courses support those paths. Further, the incredibly high student-to-faculty ratio, along with already stretched faculty resources, exacerbates the situation. There is never enough time for busy faculty to meet with their advisees.</p>
