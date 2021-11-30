60b16fe2b0f319550b0b419683d5d74a78532d6a
rpmiller18 <rpmiller@udel.edu>
2021-11-30 17:22:06 -0500

    edit_button

--- src/components/SingleSemesterDisplay.tsx
+++ src/components/SingleSemesterDisplay.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import { Semester } from "../interfaces/Semester";
 import { Course } from "../interfaces/Course";
+import {Mod} from "../components/modalHelper";
+//import handleShow from "../components/modalHelper";
 
 
 interface SS_Semester{
@@ -14,9 +16,11 @@ export function SingleSemesterDisplay({ course_set, semester_number, userSemeste
 
     //Constants-------------------
     const [creditSum, addSum] = useState(0);
+    const [showModal, setShow] = useState(false);
 
 
     //Functions-------------------
+
     
     //Removes all the courses from the current Semester
     function remove_all_classes(sem_num: number): void {
@@ -43,20 +47,24 @@ export function SingleSemesterDisplay({ course_set, semester_number, userSemeste
             }
         });
     }
-
+ 
     //Defining the display characteristics of a Course in the Single Semester View
     function Course_SS({ crsName, crsDescription, crsCredits, semester_number }:Course):JSX.Element{
+    
         return (
             <tr>
                 <td><b>{crsName}:</b> {crsDescription}</td>
                 <td className="text-center">{crsCredits}</td>
                 <td className="editcourse">
-                    <button type="button" className="btn btn-outline-secondary m-1">Edit</button>
+                
+                    <button type="button" className="btn btn-outline-secondary m-1" onClick={()=> setShow(true)}>Edit</button>                                                        
                     <button type="button" className="btn btn-outline-danger m-1" onClick={() => remove_class(semester_number as number, crsName)}>X</button>
                 </td>
             </tr>
         );
     }
+    
+    
 
 
     //Use Effect-------------------
@@ -73,21 +81,23 @@ export function SingleSemesterDisplay({ course_set, semester_number, userSemeste
     
     //Return Statement-------------
     return <div className="">
-        <table className="table">
-            <thead>
-                <h2 className="Semester"><b>Semester {semester_number}</b></h2>
-                <tr>
-                    <th className="text-center">Courses</th>
-                    <th className="text-center">Credit</th>
-                    <th className="Actions">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {course_set.map((course: Course, index: number) => {
-                    return <Course_SS key={index} crsName={course.crsName} crsDescription={course.crsDescription} crsCredits={course.crsCredits} semester_number={semester_number} />;
-                })}
-            </tbody >
-        </table >
+        <form>
+            <table className="table">
+                <thead>
+                    <h2 className="Semester"><b>Semester {semester_number}</b></h2>
+                    <tr>
+                        <th className="text-center">Courses</th>
+                        <th className="text-center">Credit</th>
+                        <th className="Actions">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {course_set.map((course: Course, index: number) => {
+                        return <Course_SS key={index} crsName={course.crsName} crsDescription={course.crsDescription} crsCredits={course.crsCredits} semester_number={semester_number} />;
+                    })}
+                </tbody >
+            </table >
+        </form>
         <div>
             <b>Total Credits:  </b>
             <b>{creditSum}</b>
@@ -98,6 +108,10 @@ export function SingleSemesterDisplay({ course_set, semester_number, userSemeste
             <button type="button" className="btn btn-danger m-1" onClick={() => remove_all_classes(semester_number as number)}>Remove all courses</button>
 
         </div>
-    </div >
-    ;
+        <div>
+            <Mod setShow={setShow} showModal={showModal}></Mod>
+        </div>
+    </div >;
+
+
 }
